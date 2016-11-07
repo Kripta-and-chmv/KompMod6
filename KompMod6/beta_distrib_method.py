a916fe365536d6e6201a8a353bf44693399d5ecf
Юрий Савицкий < >
2016-11-07 13:19:56 +0700

    п

--- KompMod6/beta_distrib_method.py
+++ KompMod6/beta_distrib_method.py
@@ -5,14 +5,25 @@ def first(u, v):
     """for u, v - integer
     """
     rand_arr = []
-    arr = np.zeros_like(dtype=float)
     for i in range(u + v - 1):
         rand_arr.append(random.random())
 
     random_arr.sort()
 
-    return 
+    return rand_arr[u-1]
 
 def second(u, v):
     """for u, v - positive
     """
+    q1 = random.random()
+    q2 = random.random()
+    q1_pow = q1**(1/u)
+    q2_pow = q2**(1/v)
+
+    while q1_pow + q2_pow > 1:
+        q1 = random.random()
+        q2 = random.random()
+        q1_pow = q1**(1/u)
+        q2_pow = q2**(1/v)
+
+    return q1_pow / (q1_pow + q2_pow)
